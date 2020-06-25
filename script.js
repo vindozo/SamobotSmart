c13fa4c85be8505ca77609c5e0e55ffbbe47b241
vindozo <vindozo@gmail.com>
2020-06-25 10:07:56 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,40 @@
+samobot = {
+  
+  lifeClock: function(){
+    if(localStorage.getItem('lifeClock') < 1) {
+      date = new Date(0);
+      localStorage.setItem('lifeBorn', (new Date()).getTime() )
+      localStorage.setItem('lifeClock', 1000);
+    } else {
+      date = new Date(localStorage.getItem('lifeClock')-0);
+      date.setMinutes(date.getMinutes() + date.getTimezoneOffset());
+      localStorage.setItem('lifeClock', (localStorage.getItem('lifeClock') - 0) + 1000)
+    }
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+    var str = Math.floor((localStorage.getItem('lifeClock')-0) / (24*60*60*1000)) + ":"
+    if(hours < 10) hours = "0" + hours;
+    if(minutes < 10) minutes = "0" + minutes;
+    if(seconds < 10) seconds = "0" + seconds;
+    str += hours + ":" + minutes + ":" + seconds;
+
+    document.getElementById("lifeClock").innerHTML = str;
+    document.getElementById("lifeBorn").innerHTML = new Date(localStorage.getItem('lifeBorn')-0).toLocaleString('ru', {
+    day:   '2-digit',
+    month: '2-digit',
+    year:  'numeric'
+  });
+    setTimeout(samobot.lifeClock, 1000);
+  },
+
+  geolocation: function(position) {
+    document.getElementById("gpsLat").innerHTML = position.coords.latitude;
+    document.getElementById("gpsLon").innerHTML = position.coords.longitude;
+  }
+}
+
+samobot.lifeClock(); 
+
+navigator.geolocation.watchPosition(samobot.geolocation)
+ 
