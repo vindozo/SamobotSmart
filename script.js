7ed667c6b7c53aec7addb8363c0095953646d2a0
vindozo <vindozo@gmail.com>
2020-06-28 11:59:46 +0500

    Update script.js

--- script.js
+++ script.js
@@ -106,6 +106,7 @@ function onDeviceReady() {
   samobot.lifeCamera();
   samobot.lifeSignal();
   navigator.compass.watchHeading(samobot.lifeMagneticHeading, samobot.magneticHeadingError, { frequency: 1000});
+  document.getElementById('uin').innerHTML = device.manufacturer + '-' +device.uuid;
 }
 
 document.addEventListener("deviceready", onDeviceReady, false);
