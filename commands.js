3213745422d60b83f62365bd08576779a1d6f6c7
vindozo <vindozo@gmail.com>
2020-08-15 18:10:37 +0500

    Create commands.js

---
+++ commands.js
@@ -0,0 +1,34 @@
+commands = {
+  /* вспышки смартфоном */
+  flash: function(arr){
+    engine.flash(arr);
+  },
+  /* вибрация смартфоном */
+  vibrate: function(arr){
+    engine.vibrate(arr);
+  },
+  /* привод вперед */
+  forward: function(param){
+  },
+  /* привод назад */
+  back: function(param){
+  },
+  /* привод влево */
+  left: function(param){
+  },
+  /* привод вправо */
+  forward: function(param){
+  },
+  /* привод вперед и влево */
+  forwardLeft: function(param1, param2){
+  },
+  /* привод вперед и вправо */
+  forwardLeft: function(param1, param2){
+  },
+  /* привод назад и влево */
+  backLeft: function(param1, param2){
+  },
+  /* привод назад и вправо */
+  backLeft: function(param1, param2){
+  },
+}
