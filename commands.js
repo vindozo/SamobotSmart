805e86b2433c602d87ca29250b59be3dd0a852c4
vindozo <vindozo@gmail.com>
2020-08-20 13:18:46 +0500

    Update commands.js

--- commands.js
+++ commands.js
@@ -14,36 +14,36 @@ commands = {
   /* аним.привод вперед */
   driveForward: function(param){
     clearTimeout(commands.driveTimerId);
-    engine.usbWrite('D1');
-    commands.driveTimerId = setTimeout("engine.usbWrite('D0');", param*1000);
+    engine.arduinoWrite('D1');
+    commands.driveTimerId = setTimeout("engine.arduinoWrite('D0');", param*1000);
   },
   /* аним.привод вперед */
   driveBack: function(param){
     clearTimeout(commands.driveTimerId);
-    engine.usbWrite('D2');
-    commands.driveTimerId = setTimeout("engine.usbWrite('D0');", param*1000);
+    engine.arduinoWrite('D2');
+    commands.driveTimerId = setTimeout("engine.arduinoWrite('D0');", param*1000);
   },
   /* привод вперед */
   forward: function(param){
     clearTimeout(commands.motorTimerId);
-    engine.usbWrite('M1');
-    commands.motorTimerId = setTimeout("engine.usbWrite('M0');", param*3);
+    engine.arduinoWrite('M1');
+    commands.motorTimerId = setTimeout("engine.arduinoWrite('M0');", param*3);
   },
   /* привод назад */
   back: function(param){
     clearTimeout(commands.motorTimerId);
-    engine.usbWrite('M2');
-    commands.motorTimerId = setTimeout("engine.usbWrite('M0');", param*3);
+    engine.arduinoWrite('M2');
+    commands.motorTimerId = setTimeout("engine.arduinoWrite('M0');", param*3);
   },
   /* привод влево */
   left: function(param){
     var angle = Math.floor(45 + (45 / param ));
-    engine.usbWrite('S'+ angle);
+    engine.arduinoWrite('S'+ angle);
   },
   /* привод вправо */
   forward: function(param){
     var angle = Math.floor(45 - (45 / param ));
-    engine.usbWrite('S'+ angle);
+    engine.arduinoWrite('S'+ angle);
   },
   /* привод вперед и влево */
   forwardLeft: function(param1, param2){
@@ -67,6 +67,6 @@ commands = {
   },
   /* светодиод */
   led: function(param){
-   engine.usbWrite('L'+param);
+   engine.arduinoWrite('L'+param);
   }
 }
