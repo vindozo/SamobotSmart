39e9862bdc3e64bec87002046ef378bb25d3b95c
vindozo <vindozo@gmail.com>
2021-01-05 19:09:46 +0500

    Update emotion.js

--- emotion.js
+++ emotion.js
@@ -5,7 +5,7 @@
     loop: function(){
       /* повторим эту функцию чезез 3-15 сек */
       setTimeout("emotion.loop()", Math.floor(Math.random() * 12 + 3) * 1000);
-      this.audio((Math.floor(Math.random() * 43) + 1) + '.mp3');
+      
       switch (Math.floor(Math.random() * 5)) {
           case 1:
             emotion.sad();
@@ -41,7 +41,6 @@
 
     off: function () { /* выключить */
       this.color('black');
-      this.audio('powerdown.mp3');
       this.e().innerHTML =
         '<rect class="eye-left" x="55" y="50" width="90" height="90" rx="20" ry="20"/>'+
         '<rect class="eye-right" x="155" y="50" width="90" height="90" rx="20" ry="20"/>';
@@ -49,7 +48,7 @@
 
     neutral: function () { /* нейтральный */
       this.color('blue');
-      this.audio('neutral.mp3');
+      this.audio('neutral' + Math.floor(Math.random() * 9) + '.mp3');
       this.e().innerHTML =
         '<rect class="eye-left" x="55" y="50" width="90" height="90" rx="20" ry="20">'+
         '<animate dur="1s" repeatCount="indefinite" attributeName="y" values="50;55;50"/><animate dur="1s" repeatCount="indefinite" attributeName="height" values="90;85;90"/></rect>'+  
@@ -57,9 +56,9 @@
         '<animate dur="1s" repeatCount="indefinite" attributeName="y" values="50;45;50"/><animate dur="1s" repeatCount="indefinite" attributeName="height" values="90;95;90"/></rect>';      
     },
 
-    surprise: function () { /* нейтральный */
+    surprise: function () { /* удивление */
       this.color('green');
-      this.audio('surprise.mp3');
+      this.audio('surprise' + Math.floor(Math.random() * 9) + '.mp3');
       this.e().innerHTML =
         '<rect class="eye-left" x="55" y="50" width="90" height="90" rx="100" ry="100">'+
         '<animate dur="1s" repeatCount="indefinite" attributeName="x" values="55;50;45;55"/></rect>'+  
@@ -67,9 +66,9 @@
         '<animate dur="1s" repeatCount="indefinite" attributeName="x" values="155;150;145;155"/></rect>';      
     },
 
-    attack: function () { /* нейтральный */
+    attack: function () { /* агрессия */
       this.color('red');
-      this.audio('attack.mp3');
+      this.audio('attack' + Math.floor(Math.random() * 11) + '.mp3');
       this.e().innerHTML =
         '<path class="eye-left" d="m54.36719,98.79297c0,-10.86758 4.38632,-15.57031 15.2539,-15.57031l60.75782,15.1875c10.86758,3.79688 14.9375,5.96835 14.9375,16.83593l-0.31641,4.75391c0,10.86758 -9.13242,20 -20,20l-50,0c-10.86758,0 -20,-9.13242 -20,-20l-0.63281,-21.20703z" >'+
         '<animateTransform attributeName="transform" type="scaleY" dur="1s" repeatCount="indefinite" values="1 1;1 1.05;1 1"></path>'+
@@ -77,9 +76,9 @@
         '<animateTransform attributeName="transform" type="scaleY" dur="1s" repeatCount="indefinite" values="1 1.05;1 1;1 1.05;"></path>';
     },
 
-    sad: function () { /* нейтральный */
+    sad: function () { /* грусть */
       this.color('blue');
-      this.audio('sad.mp3');
+      this.audio('sad' + Math.floor(Math.random() * 5) + '.mp3');
       this.e().innerHTML =
         '<path class="eye-left" d="m144.57226,39.35743c0,-10.86758 -4.31172,-15.57031 -14.99446,-15.57031l-59.72443,15.1875c-10.68274,3.79688 -14.68344,5.96835 -14.68344,16.83593l0.31103,4.75391c0,10.86758 8.97709,20 19.65983,20l49.14959,0c10.68274,0 19.65983,-9.13242 19.65983,-20l0.62205,-21.20703z" >'+
         '<animateTransform attributeName="transform" type="translate" calcMode="paced" dur="4s" repeatCount="indefinite" keyTimes="0; 0.5; 0.6; 0.9; 1" values="0 0;5 0;0 100;5 100;0 0"></path>'+
@@ -87,9 +86,9 @@
         '<animateTransform attributeName="transform" type="translate" calcMode="paced" dur="4s" repeatCount="indefinite" keyTimes="0; 0.5; 0.6; 0.9; 1" values="0 0;5 0;0 100;5 100;0 0"></path>';
     },
 
-    like: function () { /* нейтральный */
+    like: function () { /* радость */
       this.color('green');
-      this.audio('like.mp3');
+      this.audio('like' + Math.floor(Math.random() * 9) + '.mp3');
       this.e().innerHTML =
         '<path class="eye-left" d="m55,39.94141c0,-10.86758 9.13242,-20 20,-20l50,0c10.86758,0 20,9.13242 20,20l-1.58204,9.1836c-42.89883,-7.21095 -44.7539,-6.21837 -86.20312,-0.00001l-2.21484,-9.18359z" >'+
         '<animateTransform attributeName="transform" type="translate" calcMode="paced" dur="4s" repeatCount="indefinite" keyTimes="0; 0.5; 0.6; 0.9; 1" values="0 0;0 5;0 0;-5 0;0 0"></path>'+
@@ -99,4 +98,3 @@
   }
 
 setTimeout(emotion.loop, 10000);
-setTimeout(emotion.neutral, 8000);
